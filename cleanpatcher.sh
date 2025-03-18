a89ca074d83f66a4f986c4294f24de88db759d67
kinxsh <100238117+kinxsh@users.noreply.github.com>
2025-03-18 18:45:42 -0400

    Update cleanpatcher.sh

--- cleanpatcher.sh
+++ cleanpatcher.sh
@@ -1,8 +1,5 @@
 yay -S waypaper --noconfirm --needed
-curl https://raw.githubusercontent.com/kinxsh/DotsHyprland/refs/heads/main/switchwall.sh -o ./switchwall.sh.new
-chmod +x ./switchwall.sh.new
 curl https://raw.githubusercontent.com/kinxsh/DotsHyprland/refs/heads/main/config.ini -o ./config.ini.new
-sudo mv ./switchwall.sh.new ~/.config/ags/scripts/color_generation/switchwall.sh
 rm -rf ~/.config/waypaper
 mkdir ~/.config/waypaper
 sudo mv ./config.ini.new ~/.config/waypaper/config.ini
