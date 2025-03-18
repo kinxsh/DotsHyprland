4dfd7598640515e61b7cccd83812d0a869551cc2
kinxsh <100238117+kinxsh@users.noreply.github.com>
2025-03-18 17:42:54 -0400

    Add files via upload

---
+++ patcher.sh
@@ -0,0 +1,12 @@
+yay -S waypaper --noconfirm --needed
+curl https://raw.githubusercontent.com/kinxsh/DotsHyprland/refs/heads/main/switchwall.sh -o ./switchwall.sh.new
+chmod +x ./switchwall.sh.new
+curl https://raw.githubusercontent.com/kinxsh/DotsHyprland/refs/heads/main/config.ini -o ./config.ini.new
+sudo mv ./switchwall.sh.new ~/.config/ags/scripts/color_generation/switchwall.sh
+rm -rf ~/.config/waypaper
+mkdir ~/.config/waypaper
+sudo mv ./config.ini.new ~/.config/waypaper/config.ini
+echo windowrule=float,waypaper | cat ~/.config/hypr/custom/rules.conf - > ~/.config/hypr/custom/rules.conf.new
+mv ~/.config/hypr/custom/rules.conf.new ~/.config/hypr/custom/rules.conf
+echo bind = Ctrl+Super, T, exec, waypaper | cat ~/.config/hypr/custom/keybinds.conf - > ~/.config/hypr/custom/keybinds.conf.new
+mv ~/.config/hypr/custom/keybinds.conf.new ~/.config/hypr/custom/keybinds.conf
