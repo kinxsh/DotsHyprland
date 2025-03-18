132f79825e31c598d615dae14ede3dc52021f6c4
kinxsh <100238117+kinxsh@users.noreply.github.com>
2025-03-18 18:49:24 -0400

    Update patcher.sh

--- patcher.sh
+++ patcher.sh
@@ -5,5 +5,5 @@ mkdir ~/.config/waypaper
 sudo mv ./config.ini.new ~/.config/waypaper/config.ini
 echo windowrule=float,waypaper | cat ~/.config/hypr/custom/rules.conf - > ~/.config/hypr/custom/rules.conf.new
 mv ~/.config/hypr/custom/rules.conf.new ~/.config/hypr/custom/rules.conf
-echo bind = Ctrl+Super, T, exec, waypaper | cat ~/.config/hypr/custom/keybinds.conf - > ~/.config/hypr/custom/keybinds.conf.new
+echo bind = Ctrl+Alt, T, exec, waypaper | cat ~/.config/hypr/custom/keybinds.conf - > ~/.config/hypr/custom/keybinds.conf.new
 mv ~/.config/hypr/custom/keybinds.conf.new ~/.config/hypr/custom/keybinds.conf
